966d744498196273b464e735d8065252125804ac
kharle14 <enriquezkharl14@gmail.com>
2023-12-16 21:11:01 +0800

    user types

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,12 +1,12 @@
 import User from '@/models/user';
+import { CreateUser } from '@/types/user';
 import { Router } from 'express';
 
 const router = Router();
 
-router.post('/create', async (req, res) => {
+router.post('/create', async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
   //   const newUser = new User({ email, password, name });
-
   //   newUser.save();
 
   const newUser = await User.create({
