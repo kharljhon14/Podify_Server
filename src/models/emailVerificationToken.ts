ab7584197ec6970ce7e2f1b6a24b8c5e77537f38
kharle14 <enriquezkharl14@gmail.com>
2023-12-17 12:32:07 +0800

    email verification schema

---
+++ src/models/emailVerificationToken.ts
@@ -0,0 +1,30 @@
+import { Model, ObjectId, Schema, model } from 'mongoose';
+import { ref } from 'yup';
+
+interface EmailVerificationTokenDocument {
+  owner: ObjectId;
+  token: string;
+  createdAt: Date;
+}
+
+const emailVerificationTokenSchema = new Schema<EmailVerificationTokenDocument>({
+  owner: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  token: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    expires: 3600, // 60 min * 60 sec
+    default: Date.now(),
+  },
+});
+
+export default model(
+  'EmailVerificationToken',
+  emailVerificationTokenSchema
+) as Model<EmailVerificationTokenDocument>;
