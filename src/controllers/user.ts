0cfdc40831e561fe355375157a4c5957311c7665
kharle14 <enriquezkharl14@gmail.com>
2023-12-17 11:43:56 +0800

    send otp to email

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,7 +1,10 @@
+import { Response } from 'express';
+import nodemailer from 'nodemailer';
+
 import User from '@/models/user';
 import { CreateUser } from '@/types/user';
 import { CreateUserSchema } from '@/utils/validationSchema';
-import { Response } from 'express';
+import { MAILTRAP_PASSWORD, MAILTRAP_USER } from '@/utils/variables';
 
 export async function create(req: CreateUser, res: Response) {
   const { email, password, name } = req.body;
@@ -15,6 +18,17 @@ export async function create(req: CreateUser, res: Response) {
     email,
     password,
   });
+
+  const transport = nodemailer.createTransport({
+    host: 'sandbox.smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: MAILTRAP_USER,
+      pass: MAILTRAP_PASSWORD,
+    },
+  });
+
+  transport.sendMail({ to: newUser.email, from: 'auth@myapp.com', html: '<h1>1234</h1>' });
 
   res.status(201).json({ data: { newUser } });
 }
