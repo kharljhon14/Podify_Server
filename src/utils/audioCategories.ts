9431684606619687f3c4789949948626bf51e1d6
kharle14 <enriquezkharl14@gmail.com>
2024-01-01 20:15:53 +0800

    audio model

---
+++ src/utils/audioCategories.ts
@@ -0,0 +1,22 @@
+export type Category =
+  | 'Arts'
+  | 'Business'
+  | 'Education'
+  | 'Entertainment'
+  | 'Kids & Family'
+  | 'Music'
+  | 'Science'
+  | 'Tech'
+  | 'Others';
+
+export const categories = [
+  'Arts',
+  'Business',
+  'Education',
+  'Entertainment',
+  'Kids & Family',
+  'Music',
+  'Science',
+  'Tech',
+  'Others',
+];
