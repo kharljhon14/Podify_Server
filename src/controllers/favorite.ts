8bb8a62f9bee63df5d8c81f5aa05b99893cc323e
kharle14 <enriquezkharl14@gmail.com>
2024-01-11 09:04:21 +0800

    favorite router

---
+++ src/controllers/favorite.ts
@@ -0,0 +1,3 @@
+import { Request, Response } from 'express';
+
+export async function toggleFavorite(req: Request, res: Response) {}
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import './db';
 import authRouter from './routers/auth';
 import audioRouter from './routers/audio';
+import favoriteRouter from './routers/favorite';
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/auth', authRouter);
 app.use('/audio', audioRouter);
+app.use('/favorite', favoriteRouter);
 
 const PORT = 8000;
 
