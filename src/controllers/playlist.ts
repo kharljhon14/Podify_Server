0808b0f57352012dab66dec966075bca26aff01c
kharle14 <enriquezkharl14@gmail.com>
2024-01-13 21:20:58 +0800

    update playlist

--- src/controllers/playlist.ts
+++ src/controllers/playlist.ts
@@ -1,6 +1,7 @@
 import Audio from '@/models/audio';
 import Playlist from '@/models/playlist';
 import { CreatePlaylistRequest, UpdatePlaylistRequest } from '@/types/audio';
+import { error } from 'console';
 import { Response } from 'express';
 
 export async function createPlaylist(req: CreatePlaylistRequest, res: Response) {
@@ -32,4 +33,32 @@ export async function createPlaylist(req: CreatePlaylistRequest, res: Response) 
   });
 }
 
-export async function updatePlaylist(req: UpdatePlaylistRequest, res: Response) {}
+export async function updatePlaylist(req: UpdatePlaylistRequest, res: Response) {
+  const { id, title, item, visibility } = req.body;
+
+  const playlist = await Playlist.findOneAndUpdate(
+    { _id: id, owner: req.user.id },
+    { title, visibility },
+    { new: true }
+  );
+
+  if (!playlist) return res.status(404).json({ error: 'Playist not found' });
+
+  if (item) {
+    const audio = await Audio.findById(item);
+
+    if (!audio) return res.status(404).json({ error: 'Audio not found' });
+    // playlist.items.push(audio._id);
+    // await playlist.save();
+
+    await Playlist.findByIdAndUpdate(playlist._id, { $addToSet: { items: item } });
+  }
+
+  res.status(201).json({
+    playlist: {
+      id: playlist._id,
+      title: playlist.title,
+      visibility: playlist.visibility,
+    },
+  });
+}
