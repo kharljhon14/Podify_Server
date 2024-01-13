ba46ae0e68a66392d667697cb772ca2757765720
kharle14 <enriquezkharl14@gmail.com>
2024-01-13 20:56:39 +0800

    playlist controller

--- src/controllers/playlist.ts
+++ src/controllers/playlist.ts
@@ -1,3 +1,33 @@
-import { Request, Response } from 'express';
+import Audio from '@/models/audio';
+import Playlist from '@/models/playlist';
+import { CreatePlaylistRequest } from '@/types/audio';
+import { Response } from 'express';
 
-export function createPlaylist(req: Request, res: Response) {}
+export async function createPlaylist(req: CreatePlaylistRequest, res: Response) {
+  const { title, audioId, visibility } = req.body;
+  const { id } = req.user;
+
+  if (audioId) {
+    const audio = await Audio.findById(audioId);
+
+    if (!audio) return res.status(404).json({ error: 'Could not find the audio' });
+  }
+
+  const newPlaylist = new Playlist({
+    title,
+    owner: id,
+    visibility,
+  });
+
+  if (audioId) newPlaylist.items = [audioId as any];
+
+  await newPlaylist.save();
+
+  res.status(201).json({
+    playlist: {
+      id: newPlaylist._id,
+      title: newPlaylist.title,
+      visibility: newPlaylist.visibility,
+    },
+  });
+}
--- src/types/audio.ts
+++ src/types/audio.ts
@@ -1,4 +1,13 @@
 import { AudioDocument } from '@/models/audio';
+import { Request } from 'express';
 import { ObjectId } from 'mongoose';
 
 export type PopulateFavoriteList = AudioDocument<{ _id: ObjectId; name: string }>;
+
+export interface CreatePlaylistRequest extends Request {
+  body: {
+    title: string;
+    audioId: string;
+    visibility: 'public' | 'private';
+  };
+}
